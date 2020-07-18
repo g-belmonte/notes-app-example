413831acb6902deb2641beca493ad005bf103331
Guilherme Belmonte <contact@belmonte.dev>
2020-07-18 14:45:20 -0300

    [elm] Create application skeleton

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,7 +1,76 @@
-module Main exposing (main)
+module Main exposing (Model, init, Msg, update, view, subscriptions)
 
-import Html exposing (text)
+import Html exposing (..)
+import Browser
+import Browser.Navigation as Nav
+import Url
 
 
+main : Program () Model Msg
 main =
-    text "Hello world"
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
+    }
+
+
+type alias Note =
+    { title: String
+    , content: String
+    }
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    , notes : Maybe (List Note)
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
+init () url key =
+    (Model key url Nothing, Cmd.none)
+
+
+type Msg
+    = Msg1
+    | UrlRequested Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Msg1 ->
+            (model, Cmd.none)
+
+        UrlRequested urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Application Title"
+    , body =
+        [ div []
+            [ text "New Application" ]
+      ]
+    }
